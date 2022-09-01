086a4e58dc60ae35f2069fd37605eb8f0cf1cc18
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-01 19:30:16 +0800

    test: added a test for R command this time for East -> South

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -50,5 +50,16 @@ class MarsRoverTest {
         //then
         assertTrue(expectedMarsRover.equals(actualMarsRover));
     }
+    @Test
+    void should_turn_south_when_executeCommand_given_R_command() {
+        //given
+        MarsRover actualMarsRover = new MarsRover(0,0,EAST);
+        String command = LEFT;
+        MarsRover expectedMarsRover = new MarsRover(0,0,SOUTH);
+        //when
+        actualMarsRover = actualMarsRover.executeCommand(command);
+        //then
+        assertTrue(expectedMarsRover.equals(actualMarsRover));
+    }
 
 }
