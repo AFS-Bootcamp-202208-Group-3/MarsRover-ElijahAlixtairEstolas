c04f6cfbc9182b7b1b23a61f9dcf05c5c6a1f893
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-01 19:26:24 +0800

    test: added a test for another L command this time for East -> North

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -28,5 +28,16 @@ class MarsRoverTest {
         //then
         assertTrue(expectedMarsRover.equals(actualMarsRover));
     }
+    @Test
+    void should_turn_north_when_executeCommand_given_L_command() {
+        //given
+        MarsRover actualMarsRover = new MarsRover(0,0,EAST);
+        String command = LEFT;
+        MarsRover expectedMarsRover = new MarsRover(0,0,NORTH);
+        //when
+        actualMarsRover = actualMarsRover.executeCommand(command);
+        //then
+        assertTrue(expectedMarsRover.equals(actualMarsRover));
+    }
 
 }
